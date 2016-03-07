62f7da2aa5c8f7a754896038803ab2fd84ef76a4
Paul Smith <paulsmith@pobox.com>
2016-03-07 14:19:43 -0600

    Update test to check for status, headers, and body of original response

--- middleware/middleware_test.go
+++ middleware/middleware_test.go
@@ -13,21 +13,41 @@ func TestChecksumMiddleware(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	code := 418
+	headers := [][2]string{
+		{"Date", "Fri, 17 Apr 2015 19:45:39 GMT"},
+		{"Content-Type", "text/html"},
+		{"Server", "go/1.4.2"},
+		{"Last-Modified", "Mon, 13 Apr 2015 20:50:04 GMT"},
+	}
+	body := "<p>We the people …</p>"
+
 	w := httptest.NewRecorder()
 	f := func(w http.ResponseWriter, r *http.Request) {
-		for _, kv := range [][2]string{
-			{"Date", "Fri, 17 Apr 2015 19:45:39 GMT"},
-			{"Content-Type", "text/html"},
-			{"Server", "go/1.4.2"},
-			{"Last-Modified", "Mon, 13 Apr 2015 20:50:04 GMT"},
-		} {
+		w.WriteHeader(code)
+		for _, kv := range headers {
 			w.Header().Set(kv[0], kv[1])
 		}
-		fmt.Fprintf(w, "<p>We the people …</p>")
+		fmt.Fprintf(w, body)
 	}
+
 	ChecksumMiddleware(http.HandlerFunc(f)).ServeHTTP(w, req)
 
-	expected := "04dc9ef491b54c24d159cabdfd900c20d273efb6"
+	if w.Code != code {
+		t.Errorf("expected %d, got %d", code, w.Code)
+	}
+
+	for _, kv := range headers {
+		if got := w.HeaderMap.Get(kv[0]); kv[1] != got {
+			t.Errorf("%q: expected %q, got %q", kv[0], kv[1], got)
+		}
+	}
+
+	if got := w.Body.String(); body != got {
+		t.Errorf("expected %q, got %q", body, got)
+	}
+
+	expected := "814a8da9ad27cd0c0a2cea3536daa3a8b12926b3"
 	if got := w.Header().Get("X-Checksum"); expected != got {
 		t.Errorf("expected %q, got %q", expected, got)
 	}
