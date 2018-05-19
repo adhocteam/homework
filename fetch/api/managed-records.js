0c784f6ef51baa4e6cd38cea3f1b4b45745d67d2
Mark Li <markruoleili@gmail.com>
2018-05-19 12:33:24 -0700

    Refactor

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -5,22 +5,24 @@ import URI from "urijs";
 window.path = "http://localhost:3000/records";
 
 const retrieve = ({ page = 1, colors }) => {
-  const recordsURL = URI(window.path).search({
-    limit: 10,
-    offset: (page - 1) * 10,
-  });
-
-  if (colors) recordsURL.addSearch('color[]', colors);
-
-  console.log('recordsURL ----->', recordsURL.toString());
-
-  return fetch(recordsURL)
+  return fetch(recordsURL(page, colors))
     .then((response) => {
       console.log(response);
     })
     .catch((error) => {
       console.log('Request error', error);
     });
+};
+
+const recordsURL = (page, colors) => {
+  const url = URI(window.path).search({
+    limit: 10,
+    offset: (page - 1) * 10,
+  });
+
+  if (colors) url.addSearch('color[]', colors);
+
+  return url;
 };
 
 export default retrieve;
