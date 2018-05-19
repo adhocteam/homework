aa59f067982b0555601da5abf1f3401cfc3dd4a3
Mark Li <markruoleili@gmail.com>
2018-05-19 13:00:50 -0700

    Add closedprimarycount to object

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -1,33 +1,49 @@
 import fetch from "../util/fetch-fill";
 import URI from "urijs";
 
+const PRIMARY_COLORS = ["red", "blue", "yellow"];
+const ITEMS_PER_PAGE = 10;
+
 // /records endpoint
 window.path = "http://localhost:3000/records";
+
 
 const retrieve = ({ page = 1, colors }) => {
   return fetch(recordsURL(page, colors))
     .then(response => response.json())
     .then(transformRecordsData)
     .catch((error) => {
-      console.log('Request error', error);
+      console.log("Request error", error);
     });
 };
 
 const recordsURL = (page, colors) => {
   const url = URI(window.path).search({
-    limit: 10,
-    offset: (page - 1) * 10,
+    limit: ITEMS_PER_PAGE,
+    offset: (page - 1) * ITEMS_PER_PAGE,
   });
 
-  if (colors) url.addSearch('color[]', colors);
+  if (colors) url.addSearch("color[]", colors);
 
   return url;
 };
 
 const transformRecordsData = (data) => {
+  const openRecords = data
+    .filter(record => record.disposition === "open")
+    .map(record => records.isPrimary = isPrimaryColor(color));
+
+  const closedPrimaryCount = data
+    .filter(record => record.disposition === "closed" && isPrimaryColor(record.color))
+    .length;
+
   return {
     ids: data.map(record => record.id),
+    open: openRecords,
+    closedPrimaryCount: closedPrimaryCount,
   };
 };
+
+const isPrimaryColor = color => PRIMARY_COLORS.includes(color);
 
 export default retrieve;
